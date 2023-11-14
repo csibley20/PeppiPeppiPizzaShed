c432df5382cd4cf7a6a50cd75f4ce1485b46ca34
csibley20 <connor.sibley@comcast.net>
2023-11-13 23:02:47 -0500

    initial commit

---
+++ player_move.gd
@@ -0,0 +1,49 @@
+extends CharacterBody3D
+
+
+const SPEED = 7
+const JUMP_VELOCITY = 4.5
+const MOUSE_SENSIBILITY = 1200
+var mouse_relative_x = 0
+var mouse_relative_y = 0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("move_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	if Input.is_action_just_pressed("ui_pause"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation.y -= event.relative.x / MOUSE_SENSIBILITY
+		$Camera3D.rotation.x -= event.relative.y / MOUSE_SENSIBILITY
+		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+		mouse_relative_x = clamp(event.relative.x, -50, 50)
+		mouse_relative_y = clamp(event.relative.y, -50, 50)
